9a57a19ce62a65ef03f3afe50a0a5128179a3077
Todd Pratt <tpratt@protonmail.com>
2023-01-03 20:48:55 -0500

    reorganize environment settings

--- extensions.py
+++ extensions.py
@@ -1,10 +1,18 @@
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
+from settings import CORS_DOMAIN
 
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
 bcrypt = Bcrypt()
+cors = CORS(resources={
+    r".*": {
+        "origins": CORS_DOMAIN,
+        "supports_credentials": True,
+    }
+})
---
+++ settings.py
@@ -0,0 +1,5 @@
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+CORS_DOMAIN = os.getenv('CORS_DOMAIN', '*')
